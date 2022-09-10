dfbd552ead8cbe2f99d96b0e5e447d88885e61e4
Jesús García <ctw@ctwhome.com>
2022-09-10 15:53:38 +0200

    Add number of participants to the title

--- src/lib/components/Participants.svelte
+++ src/lib/components/Participants.svelte
@@ -1,14 +1,24 @@
 <script>
-  export let data
+  export let data;
 </script>
 
-<div id="participants" class="p-10 flex flex-wrap gap-10 container mx-auto my-20">
-  <h2 class="text-2xl font-medium ">Confirmed participants</h2>
+<div
+  id="participants"
+  class="p-10 flex flex-wrap gap-10 container mx-auto my-20"
+>
+  <h2 class="text-3xl font-medium ">{data.length} Confirmed Participants</h2>
   <div class="grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
     {#each data as item}
-      <a class="mt-6 hover:border-amber-500 hover:shadow-amber-500 transition-shadow rounded shadow bg-white p-4 flex flex-col items-center text-[#374151] hover:no-underline "
-         target="_blank" href={item.link}>
-        <img class="h-[80px] object-contain" src="{item.image}" alt="{item.image}"/>
+      <a
+        class="mt-6 hover:border-amber-500 hover:shadow-amber-500 transition-shadow rounded shadow bg-white p-4 flex flex-col items-center text-[#374151] hover:no-underline "
+        target="_blank"
+        href={item.link}
+      >
+        <img
+          class="h-[80px] object-contain"
+          src={item.image}
+          alt={item.image}
+        />
         <div class="w-full lg:pl-6 p-4">
           <h4 class="mb-2 text-xl font-medium font-heading">
             {#if item.name}
@@ -25,12 +35,3 @@
     {/each}
   </div>
 </div>
-
-
-
-
-
-
-
-
-
