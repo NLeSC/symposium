1c75deedd6f48edd05bff4181f2c39f7771f1267
Jesús García <ctw@ctwhome.com>
2022-09-10 15:49:50 +0200

    fix speakers mobile layout

--- src/routes/speakers/+page.svelte
+++ src/routes/speakers/+page.svelte
@@ -1,31 +1,34 @@
 <script>
   import SpeakersIntroduction from "$content/speakers.md";
-  import SvelteMarkdown from 'svelte-markdown';
+  import SvelteMarkdown from "svelte-markdown";
 
   export let data;
 </script>
 
 <div class="container mx-auto mb-16 w-full ">
   <div class=" mt-10 max-w-4xl mx-auto text-left ">
-    <SpeakersIntroduction/>
+    <SpeakersIntroduction />
   </div>
 
   <section class="container mx-auto flex flex-wrap pb-10">
-      <div class="grid gap-5 mx-auto">
-    {#each data.members as member}
-        <div class="flex p-4 items-center rounded shadow bg-white">
-          <img class="rounded-full object-contain w-[200px] h-[200px] bg-gray-800" alt={member.name} src={member.image}>
+    <div class="grid gap-5 mx-auto">
+      {#each data.members as member}
+        <div class="flex flex-wrap p-4 items-center rounded shadow bg-white">
+          <img
+            class="rounded-full object-contain w-[200px] h-[200px] bg-gray-800 mx-auto"
+            alt={member.name}
+            src={member.image}
+          />
           <div class=" p-4">
             <h4 class="mb-2 text-xl font-bold font-heading">
-              { member.name }
+              {member.name}
             </h4>
             <p class="mb-4 text-gray-700 leading-loose prose">
               <SvelteMarkdown source={member.desc} />
             </p>
           </div>
         </div>
-    {/each}
-      </div>
+      {/each}
+    </div>
   </section>
-
 </div>
